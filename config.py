76bde6bcbc2abdf018c28e6b047817cd43c73ed7
SUMIT0481 <118965746+SUMIT0481@users.noreply.github.com>
2023-03-19 16:48:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SUMIT0481/MishuXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ogvenomeditz")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/og_family_group")
