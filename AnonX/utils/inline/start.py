d4d2854aa4a5c0f315ec86ddb24f84bf2461a00c
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2023-04-06 01:21:31 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
+                text="✨ sᴏᴜʀᴄᴇ ✨", callback_data="lund_lele"
             )
         ],
      ]
